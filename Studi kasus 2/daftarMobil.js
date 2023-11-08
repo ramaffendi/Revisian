5222b90439261937c487a461754e381d5bb0ae05
RAMADHAN EFFENDI <ramadhaneffendi98@gmail.com>
2023-11-08 23:13:26 +0700

    revisian

---
+++ Studi kasus 2/daftarMobil.js
@@ -0,0 +1,45 @@
+
+let daftarMobil = [
+  {
+    merk: "honda",
+    warna: "biru",
+  },
+  {
+    merk: "suzuki",
+    warna: "hitam",
+  },
+  {
+    merk: "toyota",
+    warna: "hijau",
+  },
+];
+
+let Mobil = daftarMobil[0];
+console.log(Mobil);
+
+
+/*
+let daftarMobil1 = [
+    { 
+        merk : "honda",
+        warna : "hijau"
+    }
+];
+
+let daftarMobil2 = [
+    {
+        merk : "suzuki",
+        warna : "putih"
+    }
+]; 
+
+let daftarMobil3 = [
+    {
+        merk : "toyota",
+        warna : "merah"
+    }
+];
+
+const daftarMobil = daftarMobil1[0]
+console.log(daftarMobil)
+*/
---
+++ Studi kasus 4/daftarMobil.js
@@ -0,0 +1,19 @@
+let daftarMobil = [ {
+    merk : "honda",
+    warna : "biru" ,
+}, {
+    merk : "suzuki",
+    warna : "hitam",
+}, {
+    merk : "toyota",
+    warna : "hijau",
+} ];
+
+//console.log(daftarMobil[2]);
+
+let i = 0 ;
+while (i < daftarMobil.length) {
+  let mobil = daftarMobil[i];
+  console.log(mobil);
+  i++;
+}
